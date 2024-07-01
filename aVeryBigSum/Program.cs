35f67c8cfc63f9303ae9ce5a6d61fee27c7360ec
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-07-01 12:01:21 +0300

    aVeryBigSum Added

---
+++ aVeryBigSum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Result
+{
+   
+
+    public static long aVeryBigSum(List<long> ar)
+    {
+        long totalSum = 0;
+        foreach (long num in ar)
+        {
+            totalSum += num;
+        }
+        return totalSum;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<long> ar = Console.ReadLine().TrimEnd().Split(' ').Select(long.Parse).ToList();
+
+        long result = Result.aVeryBigSum(ar);
+
+        Console.WriteLine(result);
+    }
+}
+
