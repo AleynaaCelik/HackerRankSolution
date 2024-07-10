a95fca7167d026832d1a89e549ac74903a8fbe23
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-07-10 23:39:57 +0300

    PlusMinus Added

--- Plus_Minus/Program.cs
+++ Plus_Minus/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+        PlusMinus(arr);
+    }
+
+    static void PlusMinus(int[] arr)
+    {
+        int length = arr.Length;
+        double positiveCount = arr.Count(x => x > 0);
+        double negativeCount = arr.Count(x => x < 0);
+        double zeroCount = arr.Count(x => x == 0);
+
+        Console.WriteLine((positiveCount / length).ToString("F6"));
+        Console.WriteLine((negativeCount / length).ToString("F6"));
+        Console.WriteLine((zeroCount / length).ToString("F6"));
+    }
+}
