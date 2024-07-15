f24e659890c8c478af0fef1929bc1fa668090b26
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-07-15 23:52:59 +0300

    gradingStudents list

--- Grading_Students/Program.cs
+++ Grading_Students/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Öğrenci sayısını oku
+        int n = int.Parse(Console.ReadLine());
+
+        // Öğrenci notlarını tutmak için dizi oluştur
+        int[] grades = new int[n];
+
+        // Öğrenci notlarını oku
+        for (int i = 0; i < n; i++)
+        {
+            grades[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Notları yuvarla ve ekrana yazdır
+        for (int i = 0; i < n; i++)
+        {
+            int finalGrade = RoundGrade(grades[i]);
+            Console.WriteLine(finalGrade);
+        }
+    }
+
+    static int RoundGrade(int grade)
+    {
+        // Not 40'ın altındaysa, yuvarlanmaz
+        if (grade < 38)
+        {
+            return grade;
+        }
+
+        // En yakın üst çokluğunu bul
+        int nextMultipleOfFive = ((grade / 5) + 1) * 5;
+
+        // Eğer fark 3 veya daha küçükse, yuvarla
+        if (nextMultipleOfFive - grade < 3)
+        {
+            return nextMultipleOfFive;
+        }
+        else
+        {
+            return grade;
+        }
+    }
+}
