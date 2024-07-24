38f90f529cc95d99f5b7df53ec9a6b666c68b1ba
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-07-24 23:02:34 +0300

    Function Added

--- Climbing_the_Leaderboard/Program.cs
+++ Climbing_the_Leaderboard/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Result
+{
+    /*
+     * Complete the 'climbingLeaderboard' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY ranked
+     *  2. INTEGER_ARRAY player
+     */
+    public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+    {
+        List<int> result = new List<int>();
+
+        // Removing duplicates and sorting in descending order
+        List<int> distinctRanks = ranked.Distinct().ToList();
+
+        // Start from the end of the ranked list
+        int i = distinctRanks.Count - 1;
+
+        foreach (int score in player)
+        {
+            while (i >= 0 && score >= distinctRanks[i])
+            {
+                i--;
+            }
+            result.Add(i + 2);
+        }
+
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int rankedCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ranked = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(rankedTemp => Convert.ToInt32(rankedTemp)).ToList();
+
+        int playerCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> player = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(playerTemp => Convert.ToInt32(playerTemp)).ToList();
+
+        List<int> result = Result.climbingLeaderboard(ranked, player);
+
+        textWriter.WriteLine(string.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
