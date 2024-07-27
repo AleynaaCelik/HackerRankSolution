2e29a9e482b845e9ae38d8964f4fdbfb1770786d
AleynaaCelik <celikaleyna71@gmail.com>
2024-07-27 23:40:51 +0300

    Function Updated

--- Designer_PDF_Viewer/Program.cs
+++ Designer_PDF_Viewer/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Example input
+        int[] heights = { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+        string word = "abc";
+
+        int area = DesignerPdfViewer(heights, word);
+        Console.WriteLine(area); // Output: 9
+    }
+
+    static int DesignerPdfViewer(int[] h, string word)
+    {
+        int maxHeight = 0;
+
+        foreach (char c in word)
+        {
+            int index = c - 'a';
+            if (h[index] > maxHeight)
+            {
+                maxHeight = h[index];
+            }
+        }
+
+        return maxHeight * word.Length;
+    }
+}
