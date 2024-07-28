472e245198011d8444529614817c5c146e890b39
AleynaaCelik <celikaleyna71@gmail.com>
2024-07-28 22:52:01 +0300

    Function Added

--- Utopian_Tree/Program.cs
+++ Utopian_Tree/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+        List<int> cycles = new List<int>();
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            cycles.Add(n);
+        }
+
+        foreach (int n in cycles)
+        {
+            int result = UtopianTree(n);
+            Console.WriteLine(result);
+        }
+    }
+
+    static int UtopianTree(int n)
+    {
+        int height = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 == 1) // İlkbahar (i tekse)
+            {
+                height *= 2;
+            }
+            else // Yaz (i çiftse)
+            {
+                height += 1;
+            }
+        }
+
+        return height;
+    }
+}
