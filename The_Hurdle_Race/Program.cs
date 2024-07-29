6973e5d2d0071165dc2368106b1b2b011f6e1a65
AleynaaCelik <celikaleyna71@gmail.com>
2024-07-29 22:27:19 +0300

    Function Added

--- The_Hurdle_Race/Program.cs
+++ The_Hurdle_Race/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // İlk satırdaki girişleri okuyalım: n ve k
+        var inputs = Console.ReadLine().Split(' ');
+        int n = int.Parse(inputs[0]);
+        int k = int.Parse(inputs[1]);
+
+        // İkinci satırdaki yükseklikleri okuyalım
+        var heights = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        // En yüksek engeli bul
+        int maxHeight = heights.Max();
+
+        // Gerekli iksir sayısını hesapla
+        int dosesRequired = Math.Max(0, maxHeight - k);
+
+        // Sonucu yazdır
+        Console.WriteLine(dosesRequired);
+    }
+}
