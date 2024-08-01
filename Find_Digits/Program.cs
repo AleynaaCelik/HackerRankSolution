2c3cae19e4acafab101f0f4d8a0299256f4f4d1d
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-08-01 22:51:46 +0300

    Function Created

--- Find_Digits/Program.cs
+++ Find_Digits/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public static int findDigits(int n)
+    {
+        int count = 0;
+        int temp = n;
+
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+            if (digit != 0 && n % digit == 0)
+            {
+                count++;
+            }
+            temp /= 10;
+        }
+
+        return count;
+    }
+
+    public static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> results = new List<int>();
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            int result = findDigits(n);
+            results.Add(result);
+        }
+
+        foreach (int result in results)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
