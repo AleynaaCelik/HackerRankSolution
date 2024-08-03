f4736cf6744bcaa49d3b14f45a60bf4c44b291ed
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-08-03 22:57:17 +0300

    Function Updated

--- Library_Fine/Program.cs
+++ Library_Fine/Program.cs
@@ -63,3 +63,11 @@ class Solution
         Console.WriteLine(result);
     }
 }
+//d1, m1, y1: Kitabın iade edildiği tarih (gün, ay, yıl).
+//d2, m2, y2: Kitabın iade edilmesi gereken tarih (gün, ay, yıl).
+//Bu koşullara göre, aşağıdaki durumlar kontrol edilir:
+
+//Yıl farkı: Eğer yıl farkı varsa, ceza 10.000 Hackos olarak sabittir.
+//Ay farkı: Aynı yıl içinde ama geç bir ayda iade edilmişse, ay farkına bağlı olarak ceza 500 Hackos çarpanı ile hesaplanır.
+//Gün farkı: Aynı ay içinde ama geç bir günde iade edilmişse, gün farkına bağlı olarak ceza 15 Hackos çarpanı ile hesaplanır.
+//Zamanında veya erken iade: Ceza yoktur.
