e278c08efbc060361447b2f3910540b939ab628a
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-08-03 22:50:34 +0300

    Function Added

--- Append_and_Delete/Program.cs
+++ Append_and_Delete/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Solution
+{
+    static string AppendAndDelete(string s, string t, int k)
+    {
+        int commonLength = 0;
+
+        // Ortak prefix uzunluğunu bul
+        for (int i = 0; i < Math.Min(s.Length, t.Length); i++)
+        {
+            if (s[i] == t[i])
+                commonLength++;
+            else
+                break;
+        }
+
+        int minOperations = (s.Length - commonLength) + (t.Length - commonLength);
+
+        // Eğer gereken işlem sayısı k'dan küçükse ve (k - minOperations) çiftse ya da 
+        // `s` ve `t` nin toplam uzunluğundan daha fazla işlem yapılabiliyorsa, "Yes" döndür
+        if (minOperations == k ||
+            (minOperations < k && (k - minOperations) % 2 == 0) ||
+            (s.Length + t.Length) <= k)
+        {
+            return "Yes";
+        }
+
+        return "No";
+    }
+
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+        string t = Console.ReadLine();
+        int k = int.Parse(Console.ReadLine());
+
+        string result = AppendAndDelete(s, t, k);
+        Console.WriteLine(result);
+    }
+}
